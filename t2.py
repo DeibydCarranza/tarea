2c4deb0eaafdb0c41449544bf98bb5fbed21004c
David Carranza <72421987+DeibydCarranza@users.noreply.github.com>
2023-04-14 00:06:12 -0600

    Final

--- t2.py
+++ t2.py
@@ -1,52 +1,77 @@
 from threading import Thread, Semaphore
 import random
 import time
+import os
 
-NUM_ALUMNOS = 7
+## Constantes
+NUM_ALUMNOS = 4
 NUM_SILLAS = 3
-TTEMPO_ESPERA = 0.3
+TIEMPO_ESPERA = 0.3
 PREGUNTAS = 5
 buffer = []
 
-mut_buffer = Semaphore(1)
-señal = Semaphore(1)
+aux =0 
+
+## Semáforos
 despertar = Semaphore(0)
-impreSem = Semaphore(1)
+señal = Semaphore(1)
+impresionSem = Semaphore(1)  #Para la protección de print's
+mut_buffer = Semaphore(1)
 multiplexAlu =Semaphore(NUM_SILLAS)
 
 def alumno(id):
+    global aux
     numPreg = random.randint(1,PREGUNTAS)
+    #Considerando el número de preguntas con un inicio de 1 para cada alumno
     var = 1
-    print('\t\t\t\t - - - - Ha entrado el alumno %d' %id)
+    with impresionSem:
+        print('\t\t\t - - - - Ha entrado el alumno %d' %id)
+
     while var <= numPreg:
-        with impreSem:
-            print('El alumno %d  tiene %d preguntas' %(id,numPreg))
+        with impresionSem:
+            print(' El alumno %d  tiene %d preguntas' %(id,numPreg))
+        #Señalizando para mostrar la disponibilidad del alumno
         señal.acquire()
 
+        #Sección critica para almacenado del alumno y la cantidad de preguntas
         with mut_buffer:
             buffer.append([id,var])
-        despertar.release()
-        var += 1
-        time.sleep(TTEMPO_ESPERA)
 
-    with impreSem:
-        print('\t\t\tSaliendo alumno %d \n' %id)
+        #Se comunica con el profesor para informar que existen preguntas pendientes
+        despertar.release()
+
+        var += 1
+        time.sleep(TIEMPO_ESPERA)
+
+    with impresionSem:
+        print('\t\t\t Saliendo alumno %d \n' %id)
+
+    #La salida depende del tope de alumnos y la ejecución de estos
+    if (NUM_ALUMNOS == id+1):
+        print('\t\t\t\t\t\t PROFE DORMIDO SALIENDO')
+        #Considerar la salida forzada
+        os._exit(0)
 
 def profesor():
+    global aux
     while True:
-        if len(buffer) > 0:
-        #Despertando al profe
-            print('\tProfe despierto')
+        if (aux==0):
+            print('\t\t\t\t\t\t PROFE DORMIDO ENTRANDO')
+            aux += 1
+            
+        else:
+            print('\t Profe despierto')
             despertar.acquire()
 
+            #Sección crítica para extraer y responder las preguntas
             with mut_buffer:
                 infoPreg = buffer.pop(0)
                 contPreg=infoPreg[1]
-                print('Respondiendo la pregunta %d del alumno %d ' % (contPreg, infoPreg[0]))
+                print(' Respondiendo la pregunta %d del alumno %d ' % (contPreg, infoPreg[0]))
+                time.sleep(TIEMPO_ESPERA)
+
+            #Se notifica que se han respondido N preguntas
             señal.release()
-        else:
-            print('PROFE DORMIDO')
-            break
 
 #Por medio de un multiplex generamos la restricción de solo NUM_SILLAS dentro del cúbiculo 
 def llamada(id,multiplexAlu):
@@ -54,6 +79,7 @@ def llamada(id,multiplexAlu):
     alumno(id)
     multiplexAlu.release()
 
+#Llamada a 1 hilo de profesor y N hilos de alumnos
 Thread(target=profesor,args=[]).start()
 
 for i in range(NUM_ALUMNOS):
